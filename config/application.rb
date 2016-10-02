895ccf1d507bdddbc13c809578c4de39ebed6f7c
German <mmgerman89@gmail.com>
2016-10-02 16:22:13 -0300

    heroku

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,5 @@ module Shop
 	config.autoload_paths += %W(#{Rails.root}/lib/validators)
 	config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
 	config.assets.precompile << %W( *.eot *.svg *.ttf *.woff *.woff2 *.otf )
-	config.secret_key = 'e01a87959c6fb9670a41f1b1a4318272c9412ea8b885f39c837bb81f681635d5db5e7d04fe365b861cfeb2e60f9d8dacb0a8a4533bee1ba299474b782b897c00'
   end
 end
