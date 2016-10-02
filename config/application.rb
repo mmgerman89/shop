748c07c5df9770df9c4fafa3b1675bd4af318f21
German <mmgerman89@gmail.com>
2016-10-02 17:33:02 -0300

    heroku

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module Shop
     # -- all .rb files in that directory are automatically loaded.
 	config.autoload_paths += %W(#{Rails.root}/lib/validators)
 	config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
+	config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components', 'bootstrap-sass-official')
 	config.assets.precompile << %W( *.eot *.svg *.ttf *.woff *.woff2 *.otf )
   end
 end
