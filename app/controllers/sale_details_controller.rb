72643bbad00cdd1cf0432997b679e97ccd056354
German <mmgerman89@gmail.com>
2016-11-26 22:34:58 -0300

    edicion de venta

--- app/controllers/sale_details_controller.rb
+++ app/controllers/sale_details_controller.rb
@@ -1,5 +1,5 @@
 class SaleDetailsController < ApplicationController
-	before_action :set_sale, only: [:new, :create]
+	before_action :set_sale, only: [:new, :create, :destroy]	
 
 	def new
 		@sale_details = @sale.sale_details.build
@@ -7,23 +7,47 @@ class SaleDetailsController < ApplicationController
 	end
 
 	def create
-		sale_details = SaleDetail.new(sale_details_params)
-		if @sale.sale_details.last.nil?
-			sale_details.number = 1
-		else
-			sale_details.number = @sale.sale_details.last.number + 1
+		item_exists = false
+		item_id = params[:sale_details][:item_id].to_i
+		@sale.sale_details.each do |detail|
+			if detail.item_id == item_id
+				# Ya existe el item en la factura, agregar cantidad
+				item_exists = true
+				@sale_detail = detail
+				@saved_sale_detail = detail.id
+				break
+			end
 		end
-		@sale.sale_details << sale_details
+		if item_exists
+			@sale_detail.qty += params[:sale_details][:qty].to_i
+			@sale_detail.price = params[:sale_details][:price].to_f
+			@sale_detail.save!
+		else
+			sale_detail = SaleDetail.new(sale_details_params)
+			if @sale.sale_details.last.nil?
+				sale_detail.number = 1
+			else
+				sale_detail.number = @sale.sale_details.last.number + 1
+			end
+			@sale.sale_details << sale_detail
+		end
 		@sale.save!
 	end
 
 	def edit
+		@sale_detail = SaleDetail.find(params[:id])
 	end
 
 	def update
 	end
 
 	def destroy
+		@sale_detail = SaleDetail.find(params[:id])
+		@sale_detail.destroy
+
+		respond_to do |format|
+			format.js { render layout: false }
+		end
 	end
 
 
